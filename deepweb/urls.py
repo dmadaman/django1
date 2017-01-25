15cda2916944c0c2293147e906568b3f7347f08b
Deepthi Madamanchi <B002298@federated.fds>
2017-01-24 23:36:45 -0800

    got the /adduser to redirect to /greet

--- deepweb/urls.py
+++ deepweb/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from adduser import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^adduser/',include('adduser.urls')),
-    url(r'^greet/', include('adduser.urls')),
+    url(r'^adduser/',views.get_name, name='get_name'),
+    url(r'^greet/',views.greet, name='greet'),
 ]
